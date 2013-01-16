6684e92989cca6120907700261c086ec764a35ea
Kyle Conway <kyle.conway2@gmail.com>
2013-01-15 20:43:29 -0500

    Added initial Map View Controller

--- MyDailyReprieve/MyDailyReprieve/AppDelegate.m
+++ MyDailyReprieve/MyDailyReprieve/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MapViewController.h"
 
 @implementation AppDelegate
 
@@ -18,6 +19,11 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+
+    MapViewController *mvc = [[MapViewController alloc] init];
+    
+    [self.window addSubview:mvc.view];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ MyDailyReprieve/MyDailyReprieve/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  MyDailyReprieve
+//
+//  Created by CONWAYKS on 12/11/12.
+//  Copyright (c) 2012 CONWAYKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
